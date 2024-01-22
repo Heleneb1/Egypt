439248325f5eecb25c89527bee0ee5a5f5c54b99
Heleneb1 <helene.blanche@hotmail.fr>
2024-01-22 10:42:20 +0100

    Some new changes

--- src/main/java/com/example/egypt/EgyptApplication.java
+++ src/main/java/com/example/egypt/EgyptApplication.java
@@ -1,11 +1,14 @@
 package com.example.egypt;
 
+import com.example.egypt.config.WebMvcConfig;
 import com.example.egypt.services.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebMvcConfig.class)
 
 public class EgyptApplication {
 	private final EmailSenderService senderService;
